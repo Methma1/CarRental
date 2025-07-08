1a04fb99477897fa3d5197dd8d407098f97cecaa
Methma kaveesha <mkaveeshakulathunga@gmail.com>
2025-07-08 12:19:34 +0530

    implementing React routes,pages ,config tailwing and implemented navbar

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
+import CarDetails from './Pages/CarDetails'
+import Cars from './Pages/Cars'
+import MyBookings from './Pages/MyBookings'
+
+const App = () => {
+
+  const [ShowLogin, setShowLogin] = useState(false)
+  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+  return (
+    <>
+     { !isOwnerPath && <Navbar setShowLogin={setShowLogin}/>} 
+
+     <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/car-details/:id' element={<CarDetails/>} />
+      <Route path='/cars' element={<Cars/>} />
+      <Route path='/mybooking' element={<MyBookings/>} />
+     </Routes>
+    </>
+  )
+}
+
+export default App
