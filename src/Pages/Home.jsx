e275f2cb3e357e38638aa58f1ff814068a0406df
Methma kaveesha <mkaveeshakulathunga@gmail.com>
2025-07-10 13:15:33 +0530

    implemented banner section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../Components/Hero'
 import CarCard from '../Components/CarCard'
 import FeaturedSection from '../Components/FeaturedSection'
+import Banner from '../Components/Banner'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <FeaturedSection/>
+      <Banner/>
     </>
   )
 }
