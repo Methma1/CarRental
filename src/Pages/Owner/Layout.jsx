6187fee981f9f9b4c28da8c281e0876239f4e63b
Methma kaveesha <mkaveeshakulathunga@gmail.com>
2025-07-11 13:31:00 +0530

    implemented dashboard navbar , sidebar and layout

--- src/Pages/Owner/Layout.jsx
+++ src/Pages/Owner/Layout.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import NavbarOwner from '../../Components/Owner/NavbarOwner'
+import Sidebar from '../../Components/Owner/Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div className='flex flex-col'>
+      <NavbarOwner/>
+      <div className='flex'>
+        <Sidebar/>
+        <Outlet/>
+      </div>
+    </div>
   )
 }
 
