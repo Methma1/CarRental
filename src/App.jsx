0b229ee09e2d520352bbff0905f48734e3156a13
Methma kaveesha <mkaveeshakulathunga@gmail.com>
2025-07-11 12:43:01 +0530

    implemented Owner page and implemented owner title component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import CarDetails from './Pages/CarDetails'
 import Cars from './Pages/Cars'
 import MyBookings from './Pages/MyBookings'
 import Footer from './Components/Footer'
+import Layout from './Pages/Owner/Layout'
+import Dashboard from './Pages/Owner/Dashboard'
+import AddCar from './Pages/Owner/AddCar'
+import ManageCars from './Pages/Owner/ManageCars'
+import ManageBookings from './Pages/Owner/ManageBookings'
 
 const App = () => {
 
@@ -20,6 +25,12 @@ const App = () => {
       <Route path='/car-details/:id' element={<CarDetails/>} />
       <Route path='/cars' element={<Cars/>} />
       <Route path='/my-bookings' element={<MyBookings/>} />
+      <Route path='/owner' element={<Layout/>}>
+      <Route index element={<Dashboard/>}/>
+      <Route path='add-car' element={<AddCar/>}/>
+      <Route path='manage-cars' element={<ManageCars/>}/>
+      <Route path='manage-bookings' element={<ManageBookings/>}/>
+      </Route>
      </Routes>
      {! isOwnerPath &&<Footer/>}
      
